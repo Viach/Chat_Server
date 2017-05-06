2e0ef08ac66b506d63023ffcf08b6b94e2281afa
Viach <vch.380@gmail.com>
2017-05-06 14:14:29 +0300

    local_settings.py added

--- local_settings.py
+++ local_settings.py
@@ -1,7 +1,7 @@
 import mysql.connector
 
 # set db parameters
-connector = mysql.connector.connect(user='db_user',
-                                    password='user_pass',
-                                    host='db_host',
-                                    database='db_name')
+connector = mysql.connector.connect(user='robot00_bosyi',
+                                    password='l3nludjl',
+                                    host='91.206.201.198',
+                                    database='robot00_bosyi')

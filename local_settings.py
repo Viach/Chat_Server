5837a011c0bd47fcebf1ffbe7457c266bbcc1122
Viach <vch.380@gmail.com>
2017-05-06 14:24:02 +0300

    fixed untracked files

--- local_settings.py
+++
@@ -1,7 +0,0 @@
-import mysql.connector
-
-#  set db parameters
-connector = mysql.connector.connect(user='robot00_bosyi',
-                                    password='l3nludjl',
-                                    host='91.206.201.198',
-                                    database='robot00_bosyi')
